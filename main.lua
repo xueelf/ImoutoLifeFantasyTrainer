cc8b20ecb1e594ecec65ad8fe84e47b4e3483316
Yuki <admin@yuki.sh>
2025-10-27 17:38:54 +0800

    feat: app draw logic

---
+++ main.lua
@@ -0,0 +1,13 @@
+local function endsWith(text, suffix)
+    return text:find(suffix, nil, true) ~= nil
+end
+
+local module_path = [[;.\src\?.lua]]
+
+if not endsWith(package.path, module_path) then
+    package.path = package.path .. module_path
+end
+
+local app = require("app")
+
+app.create()
---
+++ src/app.lua
@@ -0,0 +1,33 @@
+local window = require("views.window")
+local cover = require("views.cover")
+local feature = require("views.feature")
+
+local font_name = "Lolita"
+local file_font = "assets/fonts/Lolita.ttf"
+local file_cover = "assets/images/card0002.jpg"
+local file_icon = "assets/imouto.ico"
+
+local function loadFont()
+    local stream = createMemoryStream()
+
+    stream.loadFromFile(file_font)
+    loadFontFromStream(stream)
+end
+
+local function create()
+    loadFont()
+
+    local Window = window.create()
+    local Cover = cover.create(Window)
+    local Feature = feature.create(Window)
+
+    Cover.loadImageFromFile(file_cover)
+    Window.Font.setName(font_name)
+    Window.Icon.loadFromFile(file_icon)
+    Window.centerScreen()
+    Window.show()
+end
+
+return {
+    create = create,
+}
